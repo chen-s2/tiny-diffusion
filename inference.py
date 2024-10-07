d2542e73d3e08b4db7d6ab762970cdb2b7c05232
chen-s2 <chensp2601@gmail.com>
2024-10-07 21:25:07 +0300

    amazing transition between two latents

--- inference.py
+++ inference.py
@@ -12,9 +12,9 @@ print("using model:", model_path)
 
 T = 1000
 num_intervals = T
-# image_size = 48
 image_size = 64
-batch_size = 16
+batch_size = 32
+transition_between_two_latent_values = True
 
 model = torch.load(model_path)
 device = 'cuda'
@@ -27,12 +27,25 @@ beta = torch.Tensor(beta)
 num_images_generated = batch_size
 fig, axes = plt.subplots(np.ceil(batch_size/8.0).astype('int'), np.min((num_images_generated,8)), figsize=(16, 3))
 
-x_t = torch.randn(torch.Size([batch_size, 1, image_size, image_size]), dtype=torch.float32, device=device)
+if transition_between_two_latent_values:
+    x_t = torch.randn(torch.Size([1, image_size, image_size]), dtype=torch.float32, device=device)
+    x_t = x_t.unsqueeze(0).repeat(batch_size, 1, 1, 1)
+else:
+    x_t = torch.randn(torch.Size([batch_size, 1, image_size, image_size]), dtype=torch.float32, device=device)
 generated_images = []
+
+def calculate_latent_transition(z):
+    batchsize = z.shape[0]
+    start_latent = z[0]
+    end_latent = z[batchsize-1]
+    latents_transition_interpolated = torch.stack([(1 - alpha) * start_latent + alpha * end_latent for alpha in torch.linspace(0, 1, batchsize)])
+    return latents_transition_interpolated
 
 with torch.no_grad():
     for t in tqdm(timesteps):
         z = torch.randn(torch.Size([batch_size, 1, image_size, image_size]), dtype=torch.float32, device=device)
+        if transition_between_two_latent_values:
+            z = calculate_latent_transition(z)
 
         sigma_t = torch.sqrt(beta[t])
         sigma_t = sigma_t if t != timesteps[-1] else 0
@@ -44,9 +57,6 @@ with torch.no_grad():
         alpha_t_bar = torch.prod(torch.Tensor(alpha_1_to_t_array))
 
         x_t_minus_1 = (1/torch.sqrt(alpha_t)) * (x_t - ((1 - alpha_t) / torch.sqrt(1-alpha_t_bar)) * model(x_t, t)) + sigma_t * z
-        # x_t_minus_1 = x_t - ((1 - alpha_t) / (1 - torch.sqrt(alpha_t_bar))) * model(x_t, t)  #+ 0.05 * sigma_t * z # works but the denominator is incorrect according to algo 2
-        # x_t_minus_1 = x_t - ((1 - alpha_t) / (1 - torch.sqrt(alpha_t_bar))) * model(x_t, t) + sigma_t * z
-        # x_t_minus_1 = 0.2 * (x_t - ((1 - alpha_t) / torch.sqrt(1-alpha_t_bar)) * model(x_t, t)) # + 0.1 * sigma_t * z
 
         x_t, alpha_t_bar, alpha_t, sigma_t, z = [None for i in range(5)]
         if t%100 == 0:
