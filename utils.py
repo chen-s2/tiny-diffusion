c55c6e5a42c884b6ff29b0b1907387c782744016
chen-s2 <chensp2601@gmail.com>
2024-11-21 21:24:10 +0200

    readme

--- utils.py
+++ utils.py
@@ -59,7 +59,8 @@ def create_video_from_images_dir(image_dir, fps=10, target_res=128):
 
     # Read the first image to determine the frame size
     first_image = cv2.imread(sorted_paths[0])
-    first_image = cv2.resize(first_image, (target_res, target_res))
+    if target_res:
+        first_image = cv2.resize(first_image, (target_res, target_res))
 
     height, width, layers = first_image.shape
 
@@ -70,7 +71,8 @@ def create_video_from_images_dir(image_dir, fps=10, target_res=128):
     for image_path in sorted_paths:
         # Read the image
         img = cv2.imread(image_path)
-        img = cv2.resize(img, (target_res, target_res))
+        if target_res:
+            img = cv2.resize(img, (target_res, target_res))
         if img is None:
             print(f"Warning: Unable to read {image_path}. Skipping.")
             continue
@@ -81,5 +83,29 @@ def create_video_from_images_dir(image_dir, fps=10, target_res=128):
     video.release()
     print(f"Video saved to {output_path}")
 
-image_dir = r"C:\Development\Playground\stable-diffusion\results\awesome_v6"
-create_video_from_images_dir(image_dir)
+def create_wide_background_images_from_dir(image_dir, target_res=128, out_dirname='wide_back'):
+    image_paths = list_files_with_suffix(image_dir, '.png')
+
+    # Sort the image paths by name
+    sorted_paths = sorted(image_paths, key=lambda x: os.path.basename(x))
+
+    for imgpath in sorted_paths:
+        image = cv2.imread(imgpath)
+        image = cv2.resize(image, (target_res, target_res))
+        # Create an 800x300 black background
+        background = np.zeros((250, 800, 3), dtype=np.uint8)
+
+        # Get the starting coordinates for centering the image
+        x_offset = (background.shape[1] - image.shape[1]) // 2
+        y_offset = (background.shape[0] - image.shape[0]) // 2
+
+        # Embed the image in the center of the background
+        background[y_offset:y_offset + image.shape[0], x_offset:x_offset + image.shape[1]] = image
+
+        # Save or display the result
+        cv2.imwrite(os.path.join(os.path.dirname(imgpath), out_dirname, os.path.basename(imgpath).replace('.png','_centered.png')), background)
+
+
+image_dir = r"C:\Development\Playground\stable-diffusion\results\awesome_v6\wide_back"
+create_video_from_images_dir(image_dir, target_res=None)
+# create_wide_background_images_from_dir(image_dir)
